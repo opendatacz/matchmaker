56c4a3331e717b5fc77dede5ad04a77907a2cb87
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-05-08 16:08:39 +0200

    Changed templating with partials

--- src/matchmaker/lib/template.clj
+++ src/matchmaker/lib/template.clj
@@ -1,67 +1,43 @@
 (ns matchmaker.lib.template
+  (:import [clojure.lang IPersistentVector])
   (:require [matchmaker.lib.util :refer [join-file-path]]
             [clojure.java.io :as io]
             [stencil.core :refer [render-file]]
             [stencil.loader :refer [register-template set-cache]]))
 
 ; Use for development
-
 (set-cache (clojure.core.cache/ttl-cache-factory {} :ttl 0))
 
 ; Private functions
 
+(defmacro verify
+  [])
+
 (defn- get-template-path
   "Returns resource path for @template-path vector"
   [template-path]
+  {:pre [(vector? template-path)]
+   :post [((complement nil?) (io/resource %))]}
   (str (apply join-file-path (into ["templates"] template-path)) ".mustache"))
-
-(defn- get-template
-  "Reads template from @template-path filename"
-  [template-path]
-  (let [template (get-template-path template-path)
-        template-file (io/resource template)]
-    (try
-      (assert ((complement nil?) template-file))
-      (catch AssertionError e
-        (throw (Exception. (str "Template file " template " doesn't exist.")))))
-    template-file))
-
-(defn- register-partial
-  "Load and register template partial under @partial-name."
-  [partial-file-path]
-  (let [partial-name-str (name (last partial-file-path))
-        partial-body (slurp (get-template partial-file-path))]
-    (register-template partial-name-str partial-body)))
 
 ; Public functions
 
 (defn render-template
   "Pass in a vector with @template-path (without its .mustache
   filename extension), the @data for the template (a map), and a list of
-  @partials (keywords) corresponding to like-named template filenames.
 
-  Use: (render-template [path to template-file-name] :data {:key value} :partials [:partial-file-path])"
-  [template-path & {:keys [data partials]}]
-  (let [_ (doall (map register-partial partials))] ; Nasty side-effecting registration of template partials
-    (render-file (get-template-path template-path) data)))
-
-(defn render-html
-  "Render HTML page with head and footer included."
-  [template-path & {:keys [data partials]
-                    :or {data {}}}]
-  (let [html-partials [["web" "head"]
-                       ["web" "footer"]]]
-    (render-template template-path
-                    :data data
-                    :partials (distinct (apply conj partials html-partials)))))
+  Use: (render-template [path to template-file-name] :key1 value1 :key2 value2)"
+  [^IPersistentVector template-path
+   & {:as data}]
+  (render-file (get-template-path template-path) data))
 
 (defn render-sparql
   "Render SPARQL @template-path using @data with prefixes added automatically."
-  [config template-path & {:keys [data partials]
-                           :or {data {}}}]
+  [config
+   ^IPersistentVector template-path
+   & {:as data}]
   (let [source-graph (-> config :data :source-graph)
         sample-graph (-> config :benchmark :sample :graph)]
     (render-template template-path
                      :data (merge data {:source-graph source-graph
-                                        :sample-graph sample-graph})
-                     :partials (distinct (apply conj partials [:prefixes])))))
+                                        :sample-graph sample-graph}))))
