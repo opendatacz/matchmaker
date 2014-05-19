5f732c6f730bd0cb5924fe95f8af302e6af712fc
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-05-19 14:05:42 +0200

    Change CPV expansion to include more specific (narrower) concepts

--- src/matchmaker/core/sparql.clj
+++ src/matchmaker/core/sparql.clj
@@ -23,17 +23,6 @@
                     template-path
                     :data (merge data additional-data))))
 
-(defn- match-contract-broader-cpv
-  "Matches @contract using CPV expanded to broader CPV"
-  [config contract template-path]
-  (let [sparql-params (-> config :matchmaker :sparql)
-        {:keys [cpv-broader-steps cpv-graph]} (select-keys sparql-params [:cpv-broader-steps :cpv-graph])]
-    (match-contract config
-                    contract
-                    template-path
-                    :data {:cpv-graph cpv-graph
-                           :cpv-broader-steps cpv-broader-steps})))
-
 (defn- match-business-entity
   "Matches @business-entity to relevant public contracts."
   [config business-entity template-path & {:keys [data]}]
@@ -51,19 +40,12 @@
                   contract
                   ["matchmaker" "sparql" "contract" "exact_cpv"]))
 
-(defn match-contract-bidirectional-broader-cpv
-  "Match @contract using CPV codes bidirectionally expanded to broader concepts."
+(defn match-contract-expand-to-narrower-cpv
+  "Match @contract using CPV codes unidirectionally expanded to narrower concepts."
   [config contract]
-  (match-contract-broader-cpv config
-                              contract
-                              ["matchmaker" "sparql" "contract" "bidi_broader_cpv"]))
-
-(defn match-contract-unidirectional-broader-cpv
-  "Match @contract using CPV codes unidirectionally expanded to broader concepts."
-  [config contract]
-  (match-contract-broader-cpv config
-                              contract
-                              ["matchmaker" "sparql" "contract" "broader_cpv"]))
+  (match-contract config
+                  contract
+                  ["matchmaker" "sparql" "contract" "expand_to_narrower_cpv"]))
 
 (defn match-business-entity-exact-cpv
   "Match @business-entity to relevant public contracts using exact CPV matches SPARQL query."
