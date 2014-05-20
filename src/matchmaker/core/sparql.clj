3958ac8a800d4b8b40cd4561a89be250e158262f
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-05-20 11:14:15 +0200

    Reorganized SPARQL matchmaker templates

--- src/matchmaker/core/sparql.clj
+++ src/matchmaker/core/sparql.clj
@@ -21,21 +21,21 @@
   "Match @contract using exact CPV matches SPARQL query."
   [config contract]
   (match-resource config
-                  ["matchmaker" "sparql" "contract" "exact_cpv"]
+                  ["matchmaker" "sparql" "contract" "to" "business_entity" "exact_cpv"]
                   :data {:contract contract}))
 
 (defn match-contract-expand-to-narrower-cpv
   "Match @contract using CPV codes unidirectionally expanded to narrower concepts."
   [config contract]
   (match-resource config
-                  ["matchmaker" "sparql" "contract" "expand_to_narrower_cpv"]
+                  ["matchmaker" "sparql" "contract" "to" "business_entity" "expand_to_narrower_cpv"]
                   :data {:contract contract}))
 
 (defn match-business-entity-exact-cpv
   "Match @business-entity to relevant public contracts using exact CPV matches SPARQL query."
   [config business-entity]
   (match-resource config
-                  ["matchmaker" "sparql" "business_entity" "exact_cpv"]
+                  ["matchmaker" "sparql" "business_entity" "to" "contract" "exact_cpv"]
                   :data {:business-entity business-entity}))
 
 (defn match-business-entity-expand-to-narrower-cpv
@@ -43,5 +43,5 @@
   expanded to more specific concepts."
   [config business-entity]
   (match-resource config
-                  ["matchmaker" "sparql" "business_entity" "expand_to_narrower_cpv"]
+                  ["matchmaker" "sparql" "business_entity" "to" "contract" "expand_to_narrower_cpv"]
                   :data {:business-entity business-entity}))
