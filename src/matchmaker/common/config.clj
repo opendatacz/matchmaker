7db9e31d343b3d819994f4fa3026fc07d411b1a5
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-05-28 17:11:22 +0200

    Generate metadata graph

--- src/matchmaker/common/config.clj
+++ src/matchmaker/common/config.clj
@@ -10,9 +10,9 @@
   (let [filenames [(util/join-file-path "config" "config-public.edn") filename]
         data (reduce util/deep-merge (map (comp edn/read-string slurp io/resource) filenames))
         source-graph (-> data :data :source-graph)
-        sample-graph (str source-graph
-                          (if-not (= (last source-graph) \/) \/)
-                          "benchmark")]
-    (assoc-in data [:benchmark :sample :graph] sample-graph)))
+        [sample-graph metadata-graph] (map (partial util/append-to-uri source-graph)
+                                           ["benchmark" "metadata"])]
+    (util/deep-merge data {:benchmark {:sample {:graph sample-graph}}
+                           :data {:metadata-graph metadata-graph}})))
 
 (defonce config (load-config (util/join-file-path "config" "config-private.edn")))
