cfedd538b74f4aaec7660f7b0f6d54f70db2c652
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-11-12 10:17:38 +0100

    Student's t-test, p-value statistics, and random matchmaker

--- src/matchmaker/benchmark/evaluate.clj
+++ src/matchmaker/benchmark/evaluate.clj
@@ -6,10 +6,11 @@
             [clj-http.client :as client]
             [clojure.edn :as edn]
             [incanter.charts :as charts]
+            [incanter.stats :as stats]
             [slingshot.slingshot :refer [try+]]))
 
-(declare avg-rank avg-response-time found? get-matches matches-found matches-found-in-top-10
-         mean-reciprocal-rank rank)
+(declare avg-rank avg-response-time count-business-entities found? get-matches matches-found
+         matches-found-in-top-10 mean-reciprocal-rank multiplicative-inverse rank)
 
 ; ----- Private vars -----
 
@@ -37,6 +38,15 @@
   (let [times (map :time evaluation-results)]
     (avg times)))
 
+(defn- compute-random-ranks
+  "Compute @sample-size ranks for random matchmaking given @sparql-endpoint and restriction
+  on business entities given by @count-fn."
+  [sparql-endpoint sample-size & {:keys [count-fn]}]
+  (let [business-entity-count (count-fn sparql-endpoint)
+        max-number-of-results (get-in sparql-endpoint [:config :benchmark :max-number-of-results])]
+    (map #(if (< % 100) % :infinity)
+         (repeatedly sample-size #(-> business-entity-count rand Math/floor int inc)))))
+
 (defn- count-business-entities
   "Count business entities available in configured dataset."
   [sparql-endpoint]
@@ -56,10 +66,10 @@
   (let [map-fn (if parallel? pmap map)
         get-matches-fn (fn [resource]
                          (try {:matches (util/try-times 5
-                                                         (get-matches matchmaking-endpoint
-                                                                      resource
-                                                                      :limit limit
-                                                                      :matchmaker matchmaker))
+                                                        (get-matches matchmaking-endpoint
+                                                                     resource
+                                                                     :limit limit
+                                                                     :matchmaker matchmaker))
                                 :failed? false}
                                (catch Exception _
                                  (timbre/debug (format "Matchmaking <%s> failed." resource))
@@ -78,6 +88,18 @@
   [rank]
   (not= :infinity rank))
 
+(defn- found-rank
+  "Returns 1 if @rank is found in top 100,
+  otherwise returns 0."
+  [rank]
+  (if (found? rank) 1 0))
+
+(defn- found-top-10-rank
+  "Returns 1 if @rank is found in top 10,
+  otherwise returns 0."
+  [rank]
+  (if (and (found? rank) (<= rank 10)) 1 0))
+
 (defn- get-matches
   "Get matches for @resource (URI) from @matchmaker-endpoint (URL).
   Optionally specific maximum number of matches via @limit (defaults to 10)."
@@ -88,6 +110,14 @@
                                                  :as :json-string-keys})]
     (map #(get % "@id") (-> matches :body (get "member")))))
 
+(defn- get-p-value
+  "Get p-value resulting from Student's t-test comparing collections @a and @b.
+  Optional @transform-fn can be mapped over the values of collections."
+  [a b & {:keys [transform-fn]
+          :or {transform-fn identity}}]
+  (format "%f" (:p-value (stats/t-test (map transform-fn a)
+                                       :y (map transform-fn b)))))
+  
 (defn- matches-found
   "Returns the fraction of results that found correct matches."
   [evaluation-results]
@@ -106,9 +136,13 @@
   "Compute mean reciprocal rank <http://en.wikipedia.org/wiki/Mean_reciprocal_rank>
   of @evaluation-results."
   [evaluation-results]
-  (let [multiplicative-inverse (fn [rank] (if (found? rank) (/ 1 rank) 0))
-        reciprocal-ranks (map (comp multiplicative-inverse :rank) evaluation-results)]
+  (let [reciprocal-ranks (map (comp multiplicative-inverse :rank) evaluation-results)]
     (avg reciprocal-ranks)))
+
+(defn- multiplicative-inverse
+  "Get reciprocal rank of @rank."
+  [rank]
+  (if (not= :infinity rank) (double (/ 1 rank)) 0))
 
 (defn- rank
   "Compute rank of @correct-match in @matches."
@@ -117,6 +151,19 @@
     (if (= index -1)
       :infinity
       (inc index)))) ; 1-offsetted rank
+
+(defn- top-n-curve-data
+  "Compute ratios of cases when match is found in top n positions."
+  [benchmark-results]
+  (let [ranks (map :rank benchmark-results)
+        ranks-count (count ranks)
+        rank-freqs (frequencies (filter found? ranks))
+        sum-up (fn [rank] [rank (* (/ (apply +
+                                          (map second
+                                                (filter #(<= (first %) rank) rank-freqs)))
+                                    ranks-count)
+                                   100)])]
+    (into {} (map #(sum-up (first %)) rank-freqs))))
 
 ; ----- Public functions -----
 
@@ -128,13 +175,18 @@
     (into {} (for [[k v] summed-results]
                   [k (/ v results-count)]))))
 
-(defn compute-metrics-random
-  "Compute metrics for random matchmaking given @sparql-endpoint."
-  [sparql-endpoint]
-  (let [business-entity-count (count-business-entities sparql-endpoint)
-        max-number-of-results (get-in sparql-endpoint [:config :benchmark :max-number-of-results])]
-    {:matches-found (/ max-number-of-results business-entity-count)
-     :avg-rank (/ (inc business-entity-count) 2)})) ;; FIXME
+(defn compute-random-metrics
+  "Compute metrics for random matchmaker picking from business entities
+  restricted to those counted by the @count-fn."
+  [sparql-endpoint sample-size & {:keys [count-fn]
+                                  :or {count-fn count-business-entities}}]
+  (let [random-ranks (map (partial hash-map :rank)
+                          (compute-random-ranks sparql-endpoint
+                                                sample-size
+                                                :count-fn count-fn))
+        metrics (dissoc metric-fns :avg-response-time)]
+    (into {} (for [[metric metric-fn] metrics]
+               [metric (double (metric-fn random-ranks))]))))
 
 (defn compute-metrics
   "Compute @metrics ([:metric-name]) looked up from metric-fns
@@ -156,18 +208,34 @@
                               :limit limit
                               :matchmaker matchmaker)))
 
-(defn top-n-curve-data
-  "Compute ratios of cases when match is found in top n positions."
-  [benchmark-results]
-  (let [ranks (map :rank benchmark-results)
-        ranks-count (count ranks)
-        rank-freqs (frequencies (filter found? ranks))
-        sum-up (fn [rank] [rank (* (/ (apply +
-                                          (map second
-                                                (filter #(<= (first %) rank) rank-freqs)))
-                                    ranks-count)
-                                   100)])]
-    (into {} (map #(sum-up (first %)) rank-freqs))))
+(defn get-metrics-p-values
+  "Compare metrics of evaluation results @a and @b.
+  For each metric compute p-value."
+  [a b]
+  (let [diffs (util/format-numbers (merge-with - (:metrics b) (:metrics a)))
+        results-a (apply concat (:results a))
+        results-b (apply concat (:results b))
+        ranks-a (map :rank results-a)
+        ranks-b (map :rank results-b)
+        remove-infinity-fn (fn [ranks] (filter (partial not= :infinity) ranks))]
+    {:avg-rank {:diff (:avg-rank diffs)
+                :p-value (get-p-value (remove-infinity-fn ranks-a)
+                                      (remove-infinity-fn ranks-b))}
+     :avg-response-time {:diff (:avg-response-time diffs)
+                         :p-value (get-p-value (map :time results-a)
+                                               (map :time results-b))}
+     :matches-found {:diff (:matches-found diffs)
+                     :p-value (get-p-value ranks-a
+                                           ranks-b
+                                           :transform-fn found-rank)}
+     :matches-found-in-top-10 {:diff (:matches-found-in-top-10 diffs)
+                               :p-value (get-p-value ranks-a
+                                                     ranks-b
+                                                     :transform-fn found-top-10-rank)}
+     :mean-reciprocal-rank {:diff (:mean-reciprocal-rank diffs)
+                            :p-value (get-p-value ranks-a
+                                                  ranks-b
+                                                  :transform-fn multiplicative-inverse)}}))
 
 (defn top-n-curve-chart
   "Create line chart for top N results in @benchmark-results."
