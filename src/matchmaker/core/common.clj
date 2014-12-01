22a21fd27b1d75c29827d1e0a652bef52c078e68
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-12-01 18:40:38 +0100

    Exact CPV aggregated to maximum

--- src/matchmaker/core/common.clj
+++ src/matchmaker/core/common.clj
@@ -5,12 +5,15 @@
 (def matchmakers
   "Registry of available matchmakers"
   #{"exact-cpv"
+    "exact-cpv-max"
     "exact-cpv-with-idf"
     "exact-cpv-zindex"
     "expand-to-broader-cpv"
     "expand-to-broader-cpv-with-idf"
     "expand-to-narrower-cpv"
-    "expand-to-narrower-cpv-with-idf"})
+    "expand-to-narrower-cpv-with-idf"
+    "expand-to-bidi-cpv-with-idf"
+    "expand-to-sibling-cpv-with-idf"})
 
 (def extended-matchmakers
   "Registry of matchmakers including the private ones"
